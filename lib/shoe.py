da0da756aa957a52a1d0d851a416b7b88e9b3f5c
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-02-27 20:41:49 -0500

    finish lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size= 0 ):
+        self.brand = brand
+        self.size = size
+
+    def get_int_size(self):
+        return self._size
+
+    def set_int_size(self, size):
+        if(isinstance(size, int)):
+            self._size = size
+
+        else:
+            print("size must be an integer")
+
+    size = property(get_int_size, set_int_size)
+
+    def cobble(self):
+        self.condition = "New"
+        print('Your shoe is as good as new!')
+
+
